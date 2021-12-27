1ee16522486e161da05aa4117b110d7b96efbf3c
varnitsingh <varnitsingh25@gmail.com>
2021-12-27 21:15:04 +0530

    added requests

--- studyindia.py
+++ studyindia.py
@@ -5,6 +5,8 @@ import time
 import string
 import json
 import csv
+import requests
+from bs4 import BeautifulSoup
 import pandas as pd
 
 from selenium import webdriver
@@ -89,23 +91,45 @@ class StudyIndia:
                     self.log.info("Couldn't click next page.")
                     break
 
-    def extract_details(self, url):
-        self.driver.get(url)
-        xpath = "(//div[@id='college_details-new']//table)[1]//td"
-        odd = True
-        key = 'temp'
-        data = {'url': url}
-        for i in range(len(self.driver.find_elements(By.XPATH, xpath))):
-            if odd:
-                key = self.driver.find_element(
-                    By.XPATH, f"({xpath})[{i+1}]").text.strip()
-                odd = False
+    def extract_details(self, url,count,length):
+        response = requests.get(url)
+        self.log.info(f"{count}/{length} {response.status_code} {response.reason}")
+        if response.status_code != 200:
+            if response.status_code == 404:
+                pass
             else:
-                data[key] = self.driver.find_element(
-                    By.XPATH, f"({xpath})[{i+1}]").text.strip()
-                odd = True
+                self.log.info("Sleeping for 60 seconds.")
+                time.sleep(60)
+            raise TimeoutError
+        soup = BeautifulSoup(response.content,'html.parser')
+        # print(soup.prettify)
+        rows = soup.find('div',{'id':'college_details-new'}).div.table.tbody.find_all('tr')
+        data = {
+            'url': url
+        }
+        for row in rows:
+            key = row.find_all('td')[0].text.strip()
+            value = row.find_all('td')[1].text.strip()
+            data[key] = value
 
+        # print(data)
         return data
+        # self.driver.get(url)
+        # xpath = "(//div[@id='college_details-new']//table)[1]//td"
+        # odd = True
+        # key = 'temp'
+        # data = {'url': url}
+        # for i in range(len(self.driver.find_elements(By.XPATH, xpath))):
+        #     if odd:
+        #         key = self.driver.find_element(
+        #             By.XPATH, f"({xpath})[{i+1}]").text.strip()
+        #         odd = False
+        #     else:
+        #         data[key] = self.driver.find_element(
+        #             By.XPATH, f"({xpath})[{i+1}]").text.strip()
+        #         odd = True
+
+        # return data
 
     def save_to_csv(self,data):
         keys = ['url', 'College Name', 'Type of Institution', 'Category', 'Address', 'Phone', 'Fax', 'Website', 'Approved By', 'E-Mail', 'Affiliated to', 'Sub Type of Institution']
@@ -120,28 +144,31 @@ class StudyIndia:
             writer = csv.writer(wf)
             writer.writerow(row)
 
-
     def query_colleges(self):
         colleges = self.load_json_file('database/links.json')
-        options = Options()
-        options.headless = True
-        self.driver = webdriver.Firefox(
-            options=options, executable_path='./geckodriver')
+        # options = Options()
+        # options.headless = True
+        # self.driver = webdriver.Firefox(
+        #     options=options, executable_path='./geckodriver')
         count = self.load_json_file('database/progress.json')['count']
         length = len(colleges)
         # data = self.load_json_file('database/colleges.json')
         i = 1
+        not_found = self.load_json_file('database/not_found.json')
         for college in colleges.keys():
             if i < count:
                 i += 1
                 continue
-            self.log.info(f"{count}/{length}")
+            # self.log.info()
             try:
-                data = self.extract_details(college)
+                data = self.extract_details(college,count,length)
                 self.save_to_csv(data)
-            except:
-                self.log.info('Sleeping for 5 mins.')
-                time.sleep(60*5)
+            except Exception as e:
+                self.log.warning(e)
+                not_found[college] = 1
+                self.save_json_file('database/not_found.json',not_found)
+                # self.log.info('Sleeping for 5 mins.')
+                # time.sleep(60*5)
             # self.save_json_file('database/colleges.json', data)
             count += 1
             self.save_json_file('database/progress.json', {"count": count})
@@ -159,5 +186,7 @@ class StudyIndia:
 if __name__ == '__main__':
     S = StudyIndia()
     S.query_colleges()
+    # S.extract_details('http://www.studyguideindia.com/Colleges/Management-Studies/a-&-m-institute-of-management-and-technology.html')
+
     # S.generate_csv()
     # print(len(S.load_json_file('database/colleges.json')))
